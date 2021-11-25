e8cc9cd5c36262c95bd55c7529901e479b609459
BenZen59 <benoit.zenone@gmail.com>
2021-11-25 18:13:29 +0100

    Added Gift.jsx

---
+++ src/components/Gift/Gift.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export default function Gift() {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://www.songsterr.com/a/ra/songs.json?pattern=christmas-jingle-bells-metal-version'
+      )
+      .then(({ data }) => {
+        setSongs(data);
+      });
+  }, []);
+  console.log(songs[0]);
+  return (
+    <div>
+      {songs
+        .filter((song, index) => index === 0)
+        .map((song) => {
+          return (
+            <div className="gift">
+              <p>{song.title}</p>
+              <p>{song.artist.name}</p>
+              <a href={`http://www.songsterr.com/a/wa/song?id=${song.id}`}>
+                Lien vers la partition
+              </a>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
